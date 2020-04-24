9864fe23291c39f4c312d72caf516d363f90e003
rushi7997 <ramirushi7997@gmail.com>
2020-04-24 12:31:44 -0700

    added file structure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import React from 'react';
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
 
+import { Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
 
 class App extends React.Component{
     render(){
         return (
-            <div>
+            <div className = {styles.container}> 
                 <h1>App</h1>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         );
     }
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Chart = () => {
+    return (
+        <h1>chart</h1>
+    );
+}
+export default Chart;
